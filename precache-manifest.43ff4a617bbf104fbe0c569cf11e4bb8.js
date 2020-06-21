e41998f61e5cebb45498a75ed72ae7b13ff82f8d
Shaun O'Neill <oshauneill@hotmail.com>
2020-06-21 13:47:21 -0400

    Updates

---
+++ precache-manifest.43ff4a617bbf104fbe0c569cf11e4bb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da281d594571ac829d5ad7a180c7365f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ffa94fa46ef825496d86",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "52c418a3bc960cf3fff5",
+    "url": "/my-app/static/css/main.a5654924.chunk.css"
+  },
+  {
+    "revision": "ffa94fa46ef825496d86",
+    "url": "/my-app/static/js/2.307e5030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.307e5030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c418a3bc960cf3fff5",
+    "url": "/my-app/static/js/main.412c23f0.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
